869d734c78bf431b1bc20fa1bb7c012a5dc9c237
jansdhillon <imightbejan@gmail.com>
2024-09-08 14:53:54 -0600

    some tweaks but I should clean up the logging

--- start_players.sh
+++ start_players.sh
@@ -1,7 +1,5 @@
 #!/bin/bash
 
-# Start Player 1 in the background
 java -cp target/360TAssessment-1.0-SNAPSHOT.jar com.example.player.MainPlayer1 &
 
-# Start Player 2 in the background
 java -cp target/360TAssessment-1.0-SNAPSHOT.jar com.example.player.MainPlayer2
